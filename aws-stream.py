00b023b52c0bc513493875463656a394ed0b19bf
Jonathan Nocek <jonathannocek@Jonathans-MacBook-Pro.local>
2020-01-14 10:21:04 -0600

    Listener working and uploading to S3

--- aws-stream.py
+++
@@ -1,4 +0,0 @@
-import boto3
-
-kinesis = boto3.client('kinesis') # requires AWS credentials to be present in env
-kinesis.create_stream(StreamName='twitter-stream', ShardCount=5)
--- twitter-listener.py
+++ twitter-listener.py
@@ -1,16 +1,32 @@
 import tweepy
+import boto3
+import json
 
 # Import file containing private keys to access Twitter API
 import credentials
 
+# Authenticate with Twitter API
 auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
 auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+
+streamName = 'test-stream'
+
+# Authenticate with AWS
+client = boto3.client('firehose', region_name='us-east-1',
+                          aws_access_key_id=credentials.aws_access_key_id,
+                          aws_secret_access_key=credentials.aws_secret_access_key
+                          )
+
 
 api = tweepy.API(auth)
 
 class MyStreamListener(tweepy.StreamListener):
     def on_data(self, data):
-        print(data)
+
+        client.put_record(DeliveryStreamName=streamName,Record={'Data': json.loads(data)["text"]})
+        
+        print(json.loads(data)["text"])
+
         return True
 
     def on_status(self, status):
@@ -21,7 +37,11 @@ class MyStreamListener(tweepy.StreamListener):
             #returning False in on_error disconnects the stream
             return False
 
-MyStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener)
 
-myStream.filter(track=['python'])
+if __name__ == '__main__':
+
+    listener = MyStreamListener()
+    auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
+    auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+    stream = tweepy.Stream(auth, listener)
+    stream.filter(track=['donald trump'])
